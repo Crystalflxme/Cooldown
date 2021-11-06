6cd3b94f2cb4a3e9a46bd8edb28a52931b4dbd12
Paul <43885283+Crystalflxme@users.noreply.github.com>
2021-11-06 10:46:32 +0800

    Revised code
    - Type annotations
    - Replaced tick() usage with os.clock()

--- src/Cooldown.lua
+++ src/Cooldown.lua
@@ -2,41 +2,35 @@ local Cooldown = {}
 Cooldown.__index = Cooldown
 
 -- Create a new Cooldown object
-function Cooldown.new(length)
-	local cooldown = {}
-	setmetatable(cooldown, Cooldown)
-	
-	cooldown.Length = length or 1
-	cooldown.Data = {}
-	
-	return cooldown
+function Cooldown.new(length: number)
+	return setmetatable({
+		Length = length or 1,
+		Data = {}
+	}, Cooldown)
 end
 
 -- Used to clean up any completed cooldowns
--- Useful for instances acting as keys that need to be garbage collected
 function Cooldown:Cleanup()
-	for index in pairs(self.Data) do
-		local onCooldown = self:GetStatus(index)
-		
-		if not onCooldown then
+	for index: any in pairs(self.Data) do
+		if not self:GetStatus(index) then
 			self.Data[index] = nil
 		end
 	end
 end
 
 -- Used to get information about a certain key
-function Cooldown:GetStatus(key)
-	local lastUsed = self.Data[key] or 0
-	local onCooldown = tick() - lastUsed <= self.Length
+function Cooldown:GetStatus(key: any): (boolean, number)
+	local lastUsed: number = self.Data[key] or 0
+	local onCooldown: boolean = os.clock() - lastUsed <= self.Length
 	
 	return onCooldown, lastUsed
 end
 
 -- Used to complete a task based off a cooldown
-function Cooldown:DoTask(key, callback)
-	local now = tick()
-	local onCooldown, lastUsed = self:GetStatus(key)
-	local timeLeft = lastUsed + self.Length - now
+function Cooldown:DoTask(key: any, callback: (boolean?, number?) -> any)
+	local now: number = os.clock()
+	local onCooldown: boolean, lastUsed: number = self:GetStatus(key)
+	local timeLeft: number = lastUsed + self.Length - now
 	
 	if not onCooldown then
 		self.Data[key] = now
